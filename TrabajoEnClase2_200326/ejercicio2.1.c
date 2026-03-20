d6e28e41e8f6edfc839c013b3c12bb7adfd037d3
Marina Castro <MARINA.CASTRO@ucr.ac.cr>
2026-03-20 07:57:14 -0600

    ADD: Ejercicio 2.1

---
+++ TrabajoEnClase2_200326/ejercicio2.1.c
@@ -0,0 +1,41 @@
+// Hacer un programa en C en el que la tarea de los threads sea iterar 1000 veces sobre una variable compartida y hacerle ++
+// En este caso iterar 10 000 000 veces
+// compilacion: gcc -o ejercicio2.1 ejercicio2.1.c -lpthread
+// Jimena Bejarano Sanchez C31074
+// Marina Castro Peralta C31886
+
+#include <stdio.h>
+#include <pthread.h>
+#define NUM_THREADS 5 // Número de threads a crear
+#define ITERATIONS 10000000 // Número de iteraciones que cada thread realizará
+
+long sharedVariable = 0; // Variable compartida entre los threads
+
+void* increment(void *threadid) { // Función que será ejecutada por cada thread
+    long tid;
+    tid = *(long*)threadid;
+    for (int i = 0; i < ITERATIONS; i++) {
+        sharedVariable++; // Incrementar la variable compartida
+    }
+}
+
+int main() {
+    pthread_t threads[NUM_THREADS];
+    long tids[NUM_THREADS];
+    long i;
+    
+    // Crear un nuevo thread que ejecutará la función increment
+    for (i = 0; i < NUM_THREADS; i++) {
+        tids[i] = i;
+        pthread_create(&threads[i], NULL, increment, (void*)&tids[i]);
+    }
+
+    // esperar a que todos los threads terminen
+    for (i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    printf("Valor final de la variable compartida: %ld\n", sharedVariable); // Imprimir el valor final de la variable compartida
+    pthread_exit(NULL); // Terminar el thread principal
+    
+    return 0;
+}
