a81eccc5187baf724112dc467af0954e24f2e916
Marina Castro <MARINA.CASTRO@ucr.ac.cr>
2026-04-21 09:05:55 -0600

    ADD: codigo

--- TrabajoEnClase6_210426/taskingDemo.c
+++ TrabajoEnClase6_210426/taskingDemo.c
@@ -1,5 +1,41 @@
-// Write a program that prints either ”A race car” or ”A car race” and maximize parallelism
+// Write a program that prints either "A race car" or "A car race" and maximize parallelism
 // Jimena Bejarano Sanchez C31074
 // Marina Castro Peralta C31886
 // gcc taskingDemo.c -o taskingDemo -fopenmp
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+int main() {
+    srand(time(NULL));
+    int order = rand() % 2;  // 0: race car, 1: car race
+
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            #pragma omp task
+            printf("A ");
+            #pragma omp taskwait
+
+            if (order == 0) {
+                #pragma omp task
+                printf("race ");
+                #pragma omp taskwait
+
+                #pragma omp task
+                printf("car\n");
+            } else {
+                #pragma omp task
+                printf("car ");
+                #pragma omp taskwait
+
+                #pragma omp task
+                printf("race\n");
+            }
+        }
+    }
+    return 0;
+}
