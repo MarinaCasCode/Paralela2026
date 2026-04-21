52120125fd34ce215146594c59d0a657f605f68f
Marina Castro <MARINA.CASTRO@ucr.ac.cr>
2026-04-21 08:45:46 -0600

    ADD: ejercicio tasking demo

---
+++ TrabajoEnClase6_210426/taskingDemo.c
@@ -0,0 +1,5 @@
+// Write a program that prints either ”A race car” or ”A car race” and maximize parallelism
+// Jimena Bejarano Sanchez C31074
+// Marina Castro Peralta C31886
+// gcc taskingDemo.c -o taskingDemo -fopenmp
+
